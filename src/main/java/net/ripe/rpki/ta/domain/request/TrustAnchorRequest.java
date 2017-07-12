53a50f0de50fa69153023df1d1f20424e5e88c60
Mikhail Puzanov <mpuzanov@ripe.net>
2017-07-12 17:53:02 +0200

    Clean up some code.

--- src/main/java/net/ripe/rpki/ta/domain/request/TrustAnchorRequest.java
+++ src/main/java/net/ripe/rpki/ta/domain/request/TrustAnchorRequest.java
@@ -41,55 +41,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/*
-The template:
-
-<requests.TrustAnchorRequest>
-  <creationTimestamp>1498649792639</creationTimestamp>
-  <taCertificatePublicationUri>rsync://localhost:10873/ta/</taCertificatePublicationUri>
-  <taRequests>
-    <requests.SigningRequest>
-      <requestId>6f164750-b184-44ed-aa75-f2eaf4a598c7</requestId>
-      <resourceCertificateRequest>
-        <resourceClassName>DEFAULT</resourceClassName>
-        <subjectDN>CN=4d6a2ae807f842898b4411b55ae7907180b6b240</subjectDN>
-        <subjectInformationAccess>
-          <X509CertificateInformationAccessDescriptor>
-            <method>1.3.6.1.5.5.7.48.5</method>
-            <location>rsync://localhost/online/DEFAULT/96/86d27d-9369-44b3-b3d6-3e8e8ca1bb2c/1/</location>
-          </X509CertificateInformationAccessDescriptor>
-          <X509CertificateInformationAccessDescriptor>
-            <method>1.3.6.1.5.5.7.48.10</method>
-            <location>rsync://localhost/online/DEFAULT/96/86d27d-9369-44b3-b3d6-3e8e8ca1bb2c/1/TWoq6Af4QomLRBG1WueQcYC2skA.mft</location>
-          </X509CertificateInformationAccessDescriptor>
-          <X509CertificateInformationAccessDescriptor>
-            <method>1.3.6.1.5.5.7.48.13</method>
-            <location>http://localhost:7788/notification.xml</location>
-          </X509CertificateInformationAccessDescriptor>
-        </subjectInformationAccess>
-        <ipResourceSet>0.0.0.0/0, ::</ipResourceSet>
-        <encodedSubjectPublicKey>MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhKz+AnIZWNYIEbN+iwjuFuntyg2MfWZ2
-A7+ZboLHHYZ2TCIYsf2R/ASFjdGGLCTr/QAJ0GyUideFkRgEsc8jPXPiTGBUx/gaG17nwp55shIn
-tNtHCAIR5lXYo0j5sVYNVQOXMEUSP8JSHipH2sd1h0mRkd15lwrKEumn+R0wY53wN2pfa/lo3W+8
-0dQm83KD+AGz4QmeiZxXtnLCikE4N8xnqWshmHFvzcP2PDVFxZrj490ydtE9ZoIVTXBR3/AF4GK3
-oEP7s5sgEYXzjJMpwWra+E+9Et6NrWE8LX0cXM2FbqkUmTEoIz2VDjjWOJFw9e4nUzwrUuwXp37+
-mMswMQIDAQAB</encodedSubjectPublicKey>
-      </resourceCertificateRequest>
-    </requests.SigningRequest>
-  </taRequests>
-  <siaDescriptors>
-    <X509CertificateInformationAccessDescriptor>
-      <method>1.3.6.1.5.5.7.48.13</method>
-      <location>http://localhost:7788/notification.xml</location>
-    </X509CertificateInformationAccessDescriptor>
-    <X509CertificateInformationAccessDescriptor>
-      <method>1.3.6.1.5.5.7.48.5</method>
-      <location>rsync://localhost/online/</location>
-    </X509CertificateInformationAccessDescriptor>
-  </siaDescriptors>
-</requests.TrustAnchorRequest>
-
- */
 public class TrustAnchorRequest {
 
     private static final long serialVersionUID = 1L;
@@ -98,10 +49,6 @@ public class TrustAnchorRequest {
     private final URI taCertificatePublicationUri;
     private final List<TaRequest> taRequests;
     private final X509CertificateInformationAccessDescriptor[] siaDescriptors;
-
-    public TrustAnchorRequest(URI taCertificatePublicationUri, X509CertificateInformationAccessDescriptor[] siaDescriptors, TaRequest... requests) {
-        this(taCertificatePublicationUri, siaDescriptors, Arrays.asList(requests));
-    }
 
     public TrustAnchorRequest(URI taCertificatePublicationUri, X509CertificateInformationAccessDescriptor[] siaDescriptors, List<TaRequest> taRequests) {
         this.creationTimestamp = DateTimeUtils.currentTimeMillis();
@@ -124,9 +71,5 @@ public class TrustAnchorRequest {
 
     public X509CertificateInformationAccessDescriptor[] getSiaDescriptors() {
         return siaDescriptors;
-    }
-
-    public static TrustAnchorRequest newRepublishRequest(String taRequestUri) {
-        return new TrustAnchorRequest(URI.create(taRequestUri), new X509CertificateInformationAccessDescriptor[0]);
     }
 }
