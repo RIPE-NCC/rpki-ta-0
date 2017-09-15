437f6ab47b292abea563c49686e870363add2b03
Mikhail Puzanov <mpuzanov@ripe.net>
2017-09-15 14:00:13 +0200

    Restore one options to fix NPEs.

--- src/main/java/net/ripe/rpki/ta/config/Env.java
+++ src/main/java/net/ripe/rpki/ta/config/Env.java
@@ -102,7 +102,7 @@ public class Env {
         config.setMinimumValidityPeriod(Period.months(1));
         config.setUpdatePeriod(Period.months(3));
         config.setTrustAnchorName(new X500Principal("CN=RIPE-NCC-TA-TEST"));
-//        config.setNotificationUri(URI.create("https://rrdp.ripe.net/notification.xml"));
+        config.setNotificationUri(URI.create("https://rrdp.ripe.net/notification.xml"));
         return config;
     }
 
