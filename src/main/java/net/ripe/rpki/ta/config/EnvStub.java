a206199177dcfa35f8bbecd53337321127796166
Mikhail Puzanov <misha.puzanov@gmail.com>
2019-09-09 18:11:06 +0200

    Fix license.

--- src/main/java/net/ripe/rpki/ta/config/EnvStub.java
+++ src/main/java/net/ripe/rpki/ta/config/EnvStub.java
@@ -4,22 +4,22 @@ package net.ripe.rpki.ta.config;
  * ========================LICENSE_START=================================
  * RIPE NCC Trust Anchor
  * -
- * Copyright (C) 2017 - 2019 RIPE NCC
+ * Copyright (C) 2017 RIPE NCC
  * -
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the RIPE NCC nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
