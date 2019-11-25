69f28a899983041431fb2be0d0909c444ec49c6c
Ties de Kock <ties@tiesdekock.nl>
2019-11-25 14:27:23 +0100

    Whitelist types in TAStateSerializer

--- src/main/java/net/ripe/rpki/ta/serializers/TAStateSerializer.java
+++ src/main/java/net/ripe/rpki/ta/serializers/TAStateSerializer.java
@@ -34,16 +34,25 @@ package net.ripe.rpki.ta.serializers;
  */
 
 
+import net.ripe.rpki.commons.crypto.crl.X509Crl;
 import net.ripe.rpki.commons.xml.XStreamXmlSerializerBuilder;
+import net.ripe.rpki.ta.config.Config;
 import net.ripe.rpki.ta.domain.Revocation;
 import net.ripe.rpki.ta.domain.TAState;
+import net.ripe.rpki.ta.serializers.legacy.SignedObjectTracker;
+
+import javax.security.auth.x500.X500Principal;
 
 public class TAStateSerializer extends Serializer<TAState> {
 
     protected XStreamXmlSerializerBuilder<TAState> configureBuilder(XStreamXmlSerializerBuilder<TAState> builder) {
-        builder.withAliasType("TA", TAState.class);
-        builder.withAliasType("revocation", Revocation.class);
-        return builder;
+        return builder
+                .withAliasType("TA", TAState.class)
+                .withAliasType("revocation", Revocation.class)
+                .withAllowedType(Config.class)
+                .withAllowedType(X500Principal.class)
+                .withAllowedType(X509Crl.class)
+                .withAllowedTypeHierarchy(SignedObjectTracker.class);
     }
 
     protected Class<TAState> clazz() {
