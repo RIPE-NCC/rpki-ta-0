e64e9cb509a703b234f8fc20a4c9c32cf635e361
Ties de Kock <ties@tiesdekock.nl>
2019-11-26 14:56:54 +0100

    Add tests that ensure that deserialized objects match XML

---
+++ src/test/java/net/ripe/rpki/ta/serializers/TrustAnchorResponseSerializerTest.java
@@ -0,0 +1,175 @@
+package net.ripe.rpki.ta.serializers;
+
+/*-
+ * ========================LICENSE_START=================================
+ * RIPE NCC Trust Anchor
+ * -
+ * Copyright (C) 2017 - 2019 RIPE NCC
+ * -
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the RIPE NCC nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * =========================LICENSE_END==================================
+ */
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import net.ripe.rpki.commons.crypto.CertificateRepositoryObject;
+import net.ripe.rpki.commons.crypto.cms.manifest.ManifestCms;
+import net.ripe.rpki.commons.crypto.crl.X509Crl;
+import net.ripe.rpki.commons.crypto.x509cert.X509CertificateObject;
+import net.ripe.rpki.ta.domain.TAState;
+import net.ripe.rpki.ta.domain.response.SigningResponse;
+import net.ripe.rpki.ta.domain.response.TaResponse;
+import net.ripe.rpki.ta.domain.response.TrustAnchorResponse;
+import net.ripe.rpki.ta.serializers.legacy.SignedResourceCertificate;
+import org.apache.commons.lang.StringUtils;
+import org.bouncycastle.util.encoders.Base64;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class TrustAnchorResponseSerializerTest {
+    private static final String TA_RESPONSE_PATH = "src/test/resources/ta-response.xml";
+
+    private Document document;
+
+    private TrustAnchorResponse response;
+
+    @Before
+    public void loadState() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+        final String responseXML = Files.toString(new File(TA_RESPONSE_PATH), Charsets.UTF_8);
+
+        final TrustAnchorResponseSerializer trustAnchorResponseSerializer = new TrustAnchorResponseSerializer();
+        response = trustAnchorResponseSerializer.deserialize(responseXML);
+
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        document = builder.parse(new File(TA_RESPONSE_PATH));
+    }
+
+    @Test
+    public void shouldMatchSimpleFields() throws XPathExpressionException {
+        XPath xpath = XPathFactory.newInstance().newXPath();
+
+        assertEquals(Long.valueOf(xpath.evaluate("/TrustAnchorResponse/requestCreationTimestamp", document)),
+                     response.getRequestCreationTimestamp());
+    }
+
+    @Test
+    public void shouldMatchTAResponseField() throws XPathExpressionException, URISyntaxException {
+        List<TaResponse> taResponses = response.getTaResponses();
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        XPathExpression query = xpath.compile("/TrustAnchorResponse/taResponses/SigningResponse");
+        NodeList list = (NodeList)query.evaluate(document, XPathConstants.NODESET);
+
+        // Check for equal length + identical values.
+        // implictly checks that all items are of required type.
+        assertEquals(list.getLength(), taResponses.size());
+
+        for (int i=0; i < list.getLength(); i++) {
+            SigningResponse sr = (SigningResponse)taResponses.get(i);
+            Node cur = list.item(i);
+
+            assertEquals(UUID.fromString(xpath.evaluate("requestId", cur)), sr.getRequestId());
+
+            assertEquals(xpath.evaluate("resourceClassName", cur), sr.getResourceClassName());
+            assertEquals(new URI(xpath.evaluate("publicationUri", cur)), sr.getPublicationUri());
+            assertEquals(xpath.evaluate("certificate/encoded", cur),
+                         Base64.toBase64String(sr.getCertificate().getEncoded()));
+        }
+    }
+
+    /**
+     * Check the map by getting all the entries from the XML and comparing them to the object.
+     * @throws XPathExpressionException
+     */
+    @Test
+    public void shouldMatchPublishedObjects() throws XPathExpressionException, URISyntaxException {
+        XPath xpath = XPathFactory.newInstance().newXPath();
+
+        NodeList entries = (NodeList)xpath.evaluate("/TrustAnchorResponse/publishedObjects/entry",
+                                                    document, XPathConstants.NODESET);
+
+        assertEquals(entries.getLength(), response.getPublishedObjects().size());
+
+        for (int i=0; i < entries.getLength(); i++) {
+            Node entry = entries.item(i);
+
+            URI entryURI = new URI(xpath.evaluate("uri", entry));
+
+            CertificateRepositoryObject publishedObject = response.getPublishedObjects().get(entryURI);
+            // CertificateRepositoryObject is implemeneted by multiple types.
+            String encodedCertificate = xpath.evaluate("X509ResourceCertificate/encoded", entry);
+            if (StringUtils.isEmpty(encodedCertificate)) {
+                assertFalse(publishedObject instanceof X509CertificateObject);
+            } else {
+                assertTrue(publishedObject instanceof X509CertificateObject);
+
+                X509CertificateObject xco = (X509CertificateObject)publishedObject;
+                assertEquals(encodedCertificate, Base64.toBase64String(xco.getEncoded()));
+            }
+
+            String encodedCrl = xpath.evaluate("CRL/encoded", entry);
+            if (StringUtils.isEmpty(encodedCrl)) {
+                assertFalse(publishedObject instanceof X509Crl);
+            } else {
+                assertTrue(publishedObject instanceof X509Crl);
+                X509Crl crl = (X509Crl)publishedObject;
+
+                assertEquals(encodedCrl, Base64.toBase64String(crl.getEncoded()));
+            }
+
+            String encodedManifest = xpath.evaluate("Manifest/encoded", entry);
+            if (StringUtils.isEmpty(encodedManifest)) {
+                assertFalse(publishedObject instanceof ManifestCms);
+            } else {
+                assertTrue(publishedObject instanceof ManifestCms);
+                ManifestCms mfs = (ManifestCms) publishedObject;
+
+                assertEquals(encodedManifest, Base64.toBase64String(mfs.getEncoded()));
+            }
+        }
+
+    }
+
+
+}
---
+++ src/test/java/net/ripe/rpki/ta/serializers/TrustAnchorStateSerializerTest.java
@@ -0,0 +1,206 @@
+package net.ripe.rpki.ta.serializers;
+
+/*-
+ * ========================LICENSE_START=================================
+ * RIPE NCC Trust Anchor
+ * -
+ * Copyright (C) 2017 - 2019 RIPE NCC
+ * -
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the RIPE NCC nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * =========================LICENSE_END==================================
+ */
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import net.ripe.rpki.ta.config.Config;
+import net.ripe.rpki.ta.domain.TAState;
+import net.ripe.rpki.ta.serializers.legacy.SignedManifest;
+import net.ripe.rpki.ta.serializers.legacy.SignedResourceCertificate;
+import org.apache.commons.lang.StringUtils;
+import org.bouncycastle.util.encoders.Base64;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class TrustAnchorStateSerializerTest {
+    private static final String TA_STATE_PATH = "src/test/resources/ta.xml";
+
+    private Document document;
+
+    private TAState state;
+
+    @Before
+    public void loadState() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+        final String stateXML = Files.toString(new File(TA_STATE_PATH), Charsets.UTF_8);
+
+        final TAStateSerializer trustAnchorStateSerializer = new TAStateSerializer();
+        state = trustAnchorStateSerializer.deserialize(stateXML);
+
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        document = builder.parse(new File(TA_STATE_PATH));
+    }
+
+    /**
+     * Evaluate an XPath query and return the result.
+     * @param query XPath query
+     * @return String result of query
+     * @throws XPathExpressionException
+     */
+    private String xpathQuery(String query) throws XPathExpressionException {
+        // No lambda's in Java 6 -> utility function
+        XPath xpath = XPathFactory.newInstance().newXPath();
+
+        return xpath.evaluate(query, document);
+    }
+
+    @Test
+    public void shouldMatchSimpleFields() throws XPathExpressionException {
+        assertEquals(xpathQuery("/TA/encoded"), Base64.toBase64String(state.getEncoded()));
+
+        assertEquals(xpathQuery("/TA/crl/encoded"), Base64.toBase64String(state.getCrl().getEncoded()));
+
+        assertEquals(xpathQuery("/TA/keyStorePassphrase"), state.getKeyStorePassphrase());
+        assertEquals(xpathQuery("/TA/keyStoreKeyAlias"), state.getKeyStoreKeyAlias());
+
+        assertEquals(new BigInteger(xpathQuery("/TA/lastIssuedCertificateSerial")),
+                     state.getLastIssuedCertificateSerial());
+        assertEquals(new BigInteger(xpathQuery("/TA/lastCrlSerial")), state.getLastCrlSerial());
+        assertEquals(new BigInteger(xpathQuery("/TA/lastMftSerial")), state.getLastMftSerial());
+        assertEquals(Long.valueOf(xpathQuery("/TA/lastProcessedRequestTimestamp")),
+                     state.getLastProcessedRequestTimestamp());
+
+        assertEquals(new ArrayList(), state.getPreviousTaCertificates());
+    }
+
+    @Test
+    public void shouldMatchConfigField() throws XPathExpressionException, URISyntaxException {
+        Config config = state.getConfig();
+
+        assertEquals(xpathQuery("/TA/config/trustAnchorName"), config.getTrustAnchorName().getName());
+
+        assertEquals(new URI(xpathQuery("/TA/config/taCertificatePublicationUri")),
+                     config.getTaCertificatePublicationUri());
+        assertEquals(new URI(xpathQuery("/TA/config/taProductsPublicationUri")),
+                     config.getTaProductsPublicationUri());
+        assertEquals(new URI(xpathQuery("/TA/config/notificationUri")),
+                     config.getNotificationUri());
+
+        assertEquals(xpathQuery("/TA/config/keystoreProvider"),
+                     config.getKeystoreProvider());
+        assertEquals(xpathQuery("/TA/config/keypairGeneratorProvider"),
+                                config.getKeypairGeneratorProvider());
+        assertEquals(xpathQuery("/TA/config/signatureProvider"),
+                     config.getSignatureProvider());
+        assertEquals(xpathQuery("/TA/config/keystoreType"),
+                     config.getKeystoreType());
+        assertEquals(xpathQuery("/TA/config/persistentStorageDir"),
+                     config.getPersistentStorageDir());
+        assertEquals(Period.parse(xpathQuery("/TA/config/minimumValidityPeriod")),
+                     config.getMinimumValidityPeriod());
+        assertEquals(Period.parse(xpathQuery("/TA/config/updatePeriod")),
+                     config.getUpdatePeriod());
+    }
+
+    /**
+     * Check the signedProductionCertificates tag and its children.
+     */
+    @Test
+    public void shouldMatchSignedProductionCertificates() throws XPathExpressionException {
+        List<SignedResourceCertificate> signedProductionCertificates = state.getSignedProductionCertificates();
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        NodeList list = (NodeList)xpath.evaluate("/TA/signedProductionCertificates/net.ripe.rpki.ta.serializers.legacy.SignedResourceCertificate",
+                                                 document,
+                                                 XPathConstants.NODESET);
+
+        // Check for equal length + identical values.
+        assertEquals(list.getLength(), signedProductionCertificates.size());
+
+        for (int i=0; i < list.getLength(); i++) {
+            SignedResourceCertificate src = signedProductionCertificates.get(i);
+            Node cur = list.item(i);
+
+            assertEquals(xpath.evaluate("certificateRepositoryObject/encoded", cur),
+                         Base64.toBase64String(src.getResourceCertificate().getEncoded()));
+
+            assertEquals(xpath.evaluate("fileName", cur), src.getFileName());
+            // notValidAfter is private -> excluded.
+            String revocationTime = xpath.evaluate("revocationTime", cur);
+
+            if (StringUtils.isNotEmpty(revocationTime)) {
+                assertEquals(DateTime.parse(revocationTime), src.getRevocationTime());
+            } else {
+                assertNull(src.getRevocationTime());
+            }
+        }
+    }
+
+    /**
+     * Check the signedManifests tag and its children.
+     */
+    @Test
+    public void shouldMatchManifests() throws XPathExpressionException {
+        List<SignedManifest> signedManifests = state.getSignedManifests();
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        NodeList list = (NodeList)xpath.evaluate("/TA/signedManifests/net.ripe.rpki.ta.serializers.legacy.SignedManifest",
+                                                 document,
+                                                 XPathConstants.NODESET);
+
+        // Check for equal length + identical values.
+        assertEquals(list.getLength(), signedManifests.size());
+
+        for (int i=0; i < list.getLength(); i++) {
+            SignedManifest smf = signedManifests.get(i);
+            Node cur = list.item(i);
+
+            assertEquals(xpath.evaluate("certificateRepositoryObject/encoded", cur),
+                         Base64.toBase64String(smf.getManifest().getEncoded()));
+        }
+
+    }
+}
