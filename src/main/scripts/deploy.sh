f126a26e1d96c8f8c2d5de0198b19ee56c4a1f35
Mikhail Puzanov <misha.puzanov@gmail.com>
2020-03-05 17:50:41 +0100

    Add 'deploy.sh' script.

---
+++ src/main/scripts/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# shellcheck disable=SC2006
+# shellcheck disable=SC2086
+
+set -e
+
+BOLD_FONT="\e[1m"
+GREEN_FONT="\e[92m"
+RED_FONT="\e[31m"
+PURPLE_FONT="\e[95m"
+YELLOW_FONT="\e[93m"
+RESET_FONT="\e[0m"
+
+echo -e "$BOLD_FONT"
+
+SSH_KEY=${1}
+ARTIFACT=${2}
+NODES=${3}
+
+SSH_FLAGS="-4 -oStrictHostKeyChecking=no"
+
+echo -e "$YELLOW_FONT"
+
+for node in ${NODES}
+do
+    echo ""
+    echo ""
+    echo "Start deployment on ${node}."
+
+    echo -e "$PURPLE_FONT"
+    ssh ${SSH_FLAGS} -i ${SSH_KEY} rpkideploy@${node} -C "rm -rf deploy_work_dir && mkdir deploy_work_dir"
+    scp ${SSH_FLAGS} -i ${SSH_KEY} scripts/upgrade.sh ${ARTIFACT} rpkideploy@${node}:./deploy_work_dir
+    ssh ${SSH_FLAGS} -i ${SSH_KEY} rpkideploy@${node} -C "cd ./deploy_work_dir && ./upgrade.sh ${ARTIFACT} /export/bad/apps/rpki-ripe-ncc"
+    echo -e "$YELLOW_FONT"
+
+    echo "Deployment done on ${node}."
+    echo "------------------------------------------------"
+    echo "------------------------------------------------"
+    echo ""
+    echo ""
+done
+
+echo -e "$RESET_FONT"
+exit 0
